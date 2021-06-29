ccdebc40b3d72abbc7feed1d37cec23a6c3fcb86
Gabriel Bento <Gabriel_holanda@hotmail.com.br>
2021-06-29 18:11:27 -0400

    Create views and styles

---
+++ DesignExemplos/Covid19App/Covid19App/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Covid19App.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Covid19App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
