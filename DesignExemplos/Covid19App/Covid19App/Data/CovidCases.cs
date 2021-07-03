e2c8be7885fdeda0ff0ab77ff9c44b25e9c608d2
Gabriel Bento <Gabriel_holanda@hotmail.com.br>
2021-07-03 17:40:32 -0400

    Done 🥳

---
+++ DesignExemplos/Covid19App/Covid19App/Data/CovidCases.cs
@@ -0,0 +1,12 @@
+﻿namespace Covid19App.Data
+{
+    public class CovidCase
+    {
+        public string Country { get; private set; }
+        public int ConfirmedCases { get; set; }
+        public int TotalRecovered { get; set; }
+        public int TotalDeaths { get; set; }
+
+        public CovidCase(string country) => Country = country;
+    }
+}
--- DesignExemplos/Covid19App/Covid19App/Views/CasesPage.xaml.cs
+++ DesignExemplos/Covid19App/Covid19App/Views/CasesPage.xaml.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.Collections.ObjectModel;
+using Covid19App.Data;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +9,23 @@ namespace Covid19App.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CasesPage : ContentPage
     {
+        public ObservableCollection<CovidCase> Cases { get; }
+
+        public ObservableCollection<Person> People { get; }
+
         public CasesPage()
         {
             InitializeComponent();
+            Cases = new ObservableCollection<CovidCase>()
+            {
+                new CovidCase("Brazil")    { ConfirmedCases = 50623, TotalRecovered = 10630, TotalDeaths = 1204},
+                new CovidCase("Japan")     { ConfirmedCases = 1086, TotalRecovered = 235, TotalDeaths = 40},
+                new CovidCase("Singapure") { ConfirmedCases = 456, TotalRecovered = 144, TotalDeaths = 10},
+                new CovidCase("Nepal")     { ConfirmedCases = 1452, TotalDeaths = 230, TotalRecovered = 740},
+                new CovidCase("Mexico")    { ConfirmedCases = 1000, TotalDeaths = 123, TotalRecovered = 700}
+            };
+
+            BindingContext = this;
         }
     }
 }
